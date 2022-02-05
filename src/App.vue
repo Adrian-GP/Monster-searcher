b78337cda1afe99cf913406d00a0735de29ce10f
Adrian Peña <adrianpena_94@hotmail.com>
2022-02-05 16:09:40 -0600

    Back to state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="secondary" dark>
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <router-link to="/" class="main-logo-route">
           <v-img
